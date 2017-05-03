29203b018fd0be96821d3d4c544339e206760d96
Reed Clarke <reed.clarke@bridgevine.com>
2017-05-03 15:12:21 -0400

    refactoring

--- phpInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: reed.clarke
- * Date: 9/26/2016
- * Time: 2:52 PM
- */
-echo "Testing Two";
-phpinfo();
--- pixel.js
+++ pixel.js
@@ -2,8 +2,8 @@
  * Created by reed.clarke on 4/13/2017.
  */
 
-function sendData(customerid, face_book_id, adword_id, double_click_id, opt_one, opt_two, opt_three, opt_four, opt_five) {
-    // console.log("sendData", "customerid = " + customerid);
+function sendData(face_book_id, adword_id, double_click_id, opt_one, opt_two, opt_three, opt_four, opt_five) {
+    var customerid = getCookie("customerid");
     if(customerid === "undefined") {
         customerid = "";
     }
@@ -24,19 +24,20 @@ function sendData(customerid, face_book_id, adword_id, double_click_id, opt_one,
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            // do something to response
-            // console.log("response:", this.responseText);
-            setCookie('customerid', this.responseText, 365);
+            console.log("response:", this.responseText);
+            if(this.responseText.length >= 7 && this.responseText.length <= 45) {
+                setCookie('customerid', this.responseText, 365);
+            }
         }
-        // else {
-        //     console.log("error:",this.statusText)
-        // }
+        else {
+            console.log("error:",this.statusText)
+        }
     };
     xhr.send(JSON.stringify(data));
 }
 
 function setCookie(cname, cvalue, exdays) {
-    // console.log("setCookie", cname, cvalue, exdays);
+    console.log("setCookie", cname, cvalue, exdays);
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires="+d.toUTCString();
@@ -44,7 +45,7 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 function getCookie(cname) {
-    // console.log("getCookie", cname);
+    console.log("getCookie", cname);
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
@@ -57,11 +58,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-
-function checkCookie(face_book_id, adword_id, double_click_id, opt_one, opt_two, opt_three, opt_four, opt_five) {
-     // console.log("checkCookie");
-     var customerid = getCookie("customerid");
-     // console.log("customerid = " + customerid);
-     sendData(customerid, face_book_id, adword_id, double_click_id, opt_one, opt_two, opt_three, opt_four, opt_five);
 }
